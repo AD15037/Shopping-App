9acbe5012b19f0063c07f319e307021768600c95
AD15037 <atharvdwivedi15037@gmail.com>
2025-10-28 18:23:10 +0530

    Created NavigationBarView

--- Ecommerce/Views/HomeView.swift
+++ Ecommerce/Views/HomeView.swift
@@ -8,29 +8,38 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    fileprivate var NavigationBarView: some View {
+        HStack {
+            Spacer()
+            Text("Ecommerce App")
+                .font(.system(size: 15, weight: .semibold))
+            Spacer()
+        }
+        .overlay(alignment: .trailing) {
+            Button {
+                
+            } label: {
+                ZStack {
+                    Image(systemName: "cart.fill")
+                        .foregroundStyle(.black)
+                    ZStack {
+                        Circle()
+                        Text("1")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.white)
+                        
+                    }
+                    .offset(CGSize(width: 10, height: -10))
+                }
+            }
+            .padding(.trailing)
+        }
+    }
+    
     var body: some View {
         VStack {
-            HStack {
-                Spacer()
-                Text("Ecommerce App")
-                    .font(.system(size: 15, weight: .semibold))
-                Spacer()
-            }
-            .overlay(alignment: .trailing) {
-                Button {
-                    
-                } label: {
-                    ZStack {
-                        Image(systemName: "cart.fill")
-                            .foregroundStyle(.black)
-                        ZStack {
-                            Circle()
-                        }
-                        .offset(CGSize(width: 10, height: -10))
-                    }
-                }
-                .padding(.trailing)
-            }
+            NavigationBarView
             Spacer()
         }
     }
