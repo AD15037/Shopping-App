3ddaea0075c0ebe4c880446008e23b655fd913c7
AD15037 <atharvdwivedi15037@gmail.com>
2025-10-29 15:35:50 +0530

    Created Product Row

--- Ecommerce/Views/HomeView.swift
+++ Ecommerce/Views/HomeView.swift
@@ -45,17 +45,56 @@ struct HomeView: View {
                 HStack {
                     Text("Featured")
                         .font(.system(size: 15, weight: .semibold))
-                        .padding(.leading, 30)
+                        .padding(.leading, 35)
                     Spacer()
                     NavigationLink {
                         ProductGridView()
                     } label: {
                         Text("View All")
                             .font(.system(size: 15, weight: .bold))
-                            .padding(.trailing, 30)
+                            .padding(.trailing, 35)
                     }
                 }
                 .padding(.top)
+                ScrollView(.horizontal) {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Image("backpack")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 150, height: 150)
+                            .clipShape(UnevenRoundedRectangle(cornerRadii: .init(topLeading: 8, bottomLeading: 0, bottomTrailing: 0, topTrailing: 8)))
+                            .clipped()
+                        Group {
+                            Text("Title")
+                                .font(.system(size: 15, weight: .semibold))
+                            Text("$12.00")
+                                .font(.system(size: 15))
+                            HStack {
+                                Image(systemName: "star.fill")
+                                    .font(.system(size: 14))
+                                    .foregroundStyle(.yellow)
+                                Text("4.6")
+                                    .font(.system(size: 14))
+                            }
+                        }
+                        .padding(.horizontal, 5)
+                        Spacer()
+                    }
+                    .frame(width: 150, height: 270)
+                    .overlay(alignment: .topTrailing, content: {
+                        Button(action: {
+                            
+                        }, label: {
+                            Image(systemName: "heart")
+                        })
+                        .padding(8)
+                    })
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color(uiColor: UIColor.lightGray).opacity(0.4), lineWidth: 1)
+                    }
+                }
+                .padding(.leading, 26)
                 Spacer()
             }
         }
