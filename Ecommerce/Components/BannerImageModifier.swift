6060fcd76ed41f8c9b71c85aed8b71e64ec26c85
AD15037 <atharvdwivedi15037@gmail.com>
2025-11-03 20:51:45 +0530

    Created ProductDetailView

--- Ecommerce/Components/BannerImageModifier.swift
+++ Ecommerce/Components/BannerImageModifier.swift
@@ -15,6 +15,7 @@ extension Image {
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: UIScreen.main.bounds.width, height: 250)
+            .clipped()
     }
     
 }
